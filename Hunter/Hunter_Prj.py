c60b1034dd254c1334ac1fa3a790a5afa032b22d
Hunter <psh85a@gmail.com>
2021-03-23 14:01:23 +0900

    20210323 psh 챗봇커밋

--- Hunter/Hunter_Prj.py
+++ Hunter/Hunter_Prj.py
@@ -5,17 +5,13 @@ import telegram
 #토큰을 변수에 할당
 my_token = '1715990639:AAG-WuMvwaSkvgEwzC29uTNM65JRCb6PjlI'
 
-#bot 선언
-bot = telegram.Bot(token = my_token)
+bot = telegram.Bot(token = my_token)   #bot을 선언합니다.
 
-#업데이트 내역을 받아옵니다.
-#updates = bot.getUpdates()
+updates = bot.getUpdates()  #업데이트 내역을 받아옵니다.
 
-#내역중 메세지를 출력합니다.
-#for u in updates:
-#    print(u.message)
+for u in updates :   # 내역중 메세지를 출력합니다.
+    print(u.message)
 
-#가장 최근에 온 메세지의 chat id를 할당
-chat_id = bot.getUpdates()[-1].message.chat.id
+#chat_id = bot.getUpdates()[-1].message.chat.id #가장 최근에 온 메세지의 chat id를 가져옵니다
 
-bot.sendMessage(chat_id = chat_id, text="할로")
+#bot.sendMessage(chat_id = chat_id, text="저는 봇입니다.")
