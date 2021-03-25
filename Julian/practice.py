f2453f9c8d741bae384d4d67288ebde19e163557
jasperminning <jasperminning@gmail.com>
2021-03-25 15:19:35 +0900

    20210325 add new

--- Julian/practice.py
+++ Julian/practice.py
@@ -1,8 +1,12 @@
 import sys
 
+# sys.argv : 프로그램을 실행할 때 입력된 값을 읽어 들일 수 있는 파이썬 라이브러리
 option = sys.argv[1]
 memo = sys.argv[2]
 
 print(option)
 print(memo)
 
+C:\Users\User\PycharmProjects\Hunter_Prj\Julian>python_memo_01.py -a "Gracias"
+-a
+Gracias
