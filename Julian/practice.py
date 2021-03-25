34e64d813ab94b9f2fe017917ce2f33dd0f61fcb
jasperminning <jasperminning@gmail.com>
2021-03-25 15:23:07 +0900

    20210325 add new

--- Julian/practice.py
+++ Julian/practice.py
@@ -1,3 +1,5 @@
+#C:\Users\User\PycharmProjects\Hunter_Prj\Julian\practice.py
+
 import sys
 
 # sys.argv : 프로그램을 실행할 때 입력된 값을 읽어 들일 수 있는 파이썬 라이브러리
@@ -9,4 +11,3 @@ print(memo)
 
 C:\Users\User\PycharmProjects\Hunter_Prj\Julian>python_memo_01.py -a "Gracias"
 -a
-Gracias
